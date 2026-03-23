eba3dc7791f0171ae4fe7324267119b19cc9b2fc
meowza1 <fluffypanda1love@gmail.com>
2026-03-23 17:37:10 +0200

    Fix remote URL usage and reflow ReGui source formatting

--- code editor.lua
+++ code editor.lua
@@ -1,6 +1,6 @@
 --// Libraries
-local IDEModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/depthso/Dear-ReGui/refs/heads/main/lib/ide.lua"))()
-local ReGui = loadstring(game:HttpGet('https://web.archive.org/web/20250626175723/https://raw.githubusercontent.com/depthso/Dear-ReGui/refs/heads/main/ReGui.lua'))()
+local IDEModule = require(game.ReplicatedStorage.ide)
+local ReGui = require(game.ReplicatedStorage.ReGui)
 
 --// IDE
 local IDE = IDEModule.CodeFrame.new({
@@ -18,7 +18,7 @@ ReGui:Init({
 local Window = ReGui:Window({
 	Title = "Code editor",
 	Size = UDim2.fromOffset(300, 200),
-	NoScroll = true 
+	NoScroll = true
 })
 
 --// Add IDE frame
